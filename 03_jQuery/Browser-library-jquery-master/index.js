62579dfab584fc0f9ccab7694d1166a0ff60ffcf
FlorianSdorra <fsd114@posteo.de>
2019-11-05 15:39:43 +0100

    completed the on.click functions and on.mpusover functions in index.js

--- 03_jQuery/Browser-library-jquery-master/index.js
+++ 03_jQuery/Browser-library-jquery-master/index.js
@@ -1,1 +1,17 @@
-// Work on your tasks here
+$(document).ready(function () {
+    $('section.info__form').on('click', 'input.submit', function () {
+        $('.modal').show();
+    })
+    $('.modal-close').on('click', function () {
+        $('.modal').hide();
+    })
+    $('.teapot').on('click', function () {
+
+        $(this).toggleClass('teapot-rotate');
+    })
+    let phrases = ['Great tea for a cold night', 'Rich in flavors', 'Nice tea'];
+    $('.tea-desc').on('mouseover', function () {
+        let str = phrases[Math.floor(Math.random() * 3)];
+        $(this).text(str);
+    })
+});
